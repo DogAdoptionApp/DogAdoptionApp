8bc3797157330630f66f3ee3a2c0939bfff2f959
Robert Tipton <robbytiptontol@gmail.com>
2022-05-06 23:04:54 -0400

    Committing progress towared boilerplate. 5/6/22.

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express.app();
+const PORT = 3000;
+
+//app.get
+
+//app.post
+
+//app.delete & app.patch for stretch
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
