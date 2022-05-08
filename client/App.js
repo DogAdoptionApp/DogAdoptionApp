b0be1320ff5e06ae1f08f90c566a5e1607c5d662
Eric Komatsu <komatsueric@gmail.com>
2022-05-07 18:12:33 -0700

    Main Page in Process

--- client/App.js
+++ client/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
-import Navbar from './component/NavBar.js'
+import Navbar from "./component/NavBar.js";
+import MainCard from "./component/MainCard.js";
+import MainContainer from "./component/MainContainer.js";
 
 const App = (props) => {
-
   return (
     <>
-    <Navbar />
-    HELLO FROM APP COMPONENT
-    <Link to="/likedCard">LikeCard</Link>
-
+        <Navbar />
+        HELLO FROM APP COMPONENT
+        <MainCard />
+        <Link to="/likedCard">LikeCard</Link>
     </>
   );
 };
