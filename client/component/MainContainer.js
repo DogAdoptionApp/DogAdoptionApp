f717e8ca5c89c6db8cf5c43fbfda2ef6a87dd2bb
KB <nkbains10@gmail.com>
2022-05-10 08:40:09 -0700

    merge issues resolved. added comments and page load api back

--- client/component/MainContainer.js
+++ client/component/MainContainer.js
@@ -65,10 +65,10 @@ const MainContainer = (props) => {
       headers: {
         "Content-Type": "application/json",
       },
-    }).then((data) => console.log(data));
+    }).then((data) => console.log(data)); //need to set prefernces from db to state and then call getDogsFromApi
   };
   const getDogsFromApi = () => {
-    //get preferences from server if none deafult to all dogs
+    //get preferences from state and concat a string with query params to send to petfinder API. if none deafult to all dogs
     fetch("https://api.petfinder.com/v2/animals?type=dog&limit=100", {
       headers: {
         Authorization: "Bearer " + token,
@@ -93,22 +93,23 @@ const MainContainer = (props) => {
         client_secret: secret,
       };
 
-      // fetch("https://api.petfinder.com/v2/oauth2/token", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify(data),
-      // })
-      //   .then((response) => response.json())
-      //   .then((data) => {
-      //     console.log("Success", data);
-      //     token = data.access_token;
-      //     getDogsFromApi();
-      //   })
-      //   .catch((error) => {
-      //     console.log("Error", error);
-      //   });
+      fetch("https://api.petfinder.com/v2/oauth2/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success", data);
+          token = data.access_token;
+          //need to call setPreferencesToState and then inside that function call getDogsFromApi
+          getDogsFromApi();
+        })
+        .catch((error) => {
+          console.log("Error", error);
+        });
 
       // second call is the query +> pass in access_token and preferences
     },
